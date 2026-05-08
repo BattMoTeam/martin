fc7d271a9c22d54196828191d28f3a69bea51005
Xavier Raynaud <xavier.raynaud@sintef.no>
2026-05-08 11:36:17 +0200

    more in runPhaseField

--- Electrochemistry/PhaseField/runPhaseField.m
+++ Electrochemistry/PhaseField/runPhaseField.m
@@ -1,7 +1,17 @@
+%% read model parameters
+%
+
 filename = fullfile(battmoDir(), 'Electrochemistry', 'PhaseField', 'jsonfiles', 'phasefield.json');
 jsonstruct = parseBattmoJson(filename);
 
+%% Setup model, initial state
+%
+
 simsetup = setupPhaseFieldSimulation(jsonstruct);
+
+%% Setup schedule
+% in this case only the time steps are given, no source term
+%
 
 total = 1*hour;
 
@@ -9,12 +19,15 @@ n   = 100;
 dt  = total/n;
 dts = rampupTimesteps(total, dt, 5);
 
-control  = struct('src', srcfunc);
+control  = struct('src', []);
 
 step = struct('val', dts, 'control', ones(numel(dts), 1));
 schedule = struct('control', control, 'step', step);
 
 simsetup.schedule = schedule;
+
+%% run simulation
+%
 
 simsetup.model.verbose = true;
 
