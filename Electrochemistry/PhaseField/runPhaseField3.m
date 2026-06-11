f9e4721b8aa5332d418fba7a8d9b3c1b23140eac
Xavier Raynaud <xavier.raynaud@sintef.no>
2026-06-11 14:47:51 +0200

    added runPhaseField3

---
+++ Electrochemistry/PhaseField/runPhaseField3.m
@@ -0,0 +1,23 @@
+%% material properties
+jsonfilename = fullfile('ParameterData', 'BatteryCellParameters', 'LithiumIonBatteryCell', ...
+                        'lithium_ion_battery_lnmo_graphite.json');
+jsonstruct_material = parseBattmoJson(jsonfilename);
+
+filename = fullfile(battmoDir(), 'Electrochemistry', 'PhaseField', 'jsonfiles', 'phasefield.json');
+jsonstruct_phasefield = parseBattmoJson(filename);
+
+jsonstruct_material.PositiveElectrode.Coating.ActiveMaterial.diffusionModelType = 'phasefield';
+jsonstruct_material.PositiveElectrode.Coating.ActiveMaterial.SolidDiffusion = jsonstruct_phasefield;
+
+%% Control
+% We load the json structure for the geometrical properties
+jsonfilename = fullfile('Examples', 'JsonDataFiles', 'cc_discharge_control.json');
+jsonstruct_control = parseBattmoJson(jsonfilename);
+
+jsonstruct = mergeJsonStructs({jsonstruct_material, ...
+                               jsonstruct_control});
+
+model = setupModelFromJson(jsonstruct);
+
+cgit = model.cgit;
+
